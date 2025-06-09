4b9c2c31e13b1edb6190a0f56592119bf5d55532
Rizky Tri Mardiansyah <rizkytrimardiansyah08@gmail.com>
2025-06-09 15:22:48 +0700

    fix: update route handler types with searchParams

--- app/api/admin/category/[id]/route.ts
+++ app/api/admin/category/[id]/route.ts
@@ -1,28 +1,31 @@
 import { prisma } from "@/lib/prisma";
-import { type NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+
+type RouteParams = {
+  params: {
+    id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export async function PATCH(
   request: NextRequest,
-  context: { params: { id: string } }
-): Promise<Response> {
+  { params, searchParams }: RouteParams
+) {
   const { name } = await request.json();
   const category = await prisma.category.update({
-    where: { id: context.params.id },
+    where: { id: params.id },
     data: { name },
   });
-  return new Response(JSON.stringify(category), {
-    headers: { "Content-Type": "application/json" },
-  });
+  return Response.json(category);
 }
 
 export async function DELETE(
   request: NextRequest,
-  context: { params: { id: string } }
-): Promise<Response> {
+  { params, searchParams }: RouteParams
+) {
   await prisma.category.delete({
-    where: { id: context.params.id },
+    where: { id: params.id },
   });
-  return new Response(JSON.stringify({ success: true }), {
-    headers: { "Content-Type": "application/json" },
-  });
+  return Response.json({ success: true });
 }
