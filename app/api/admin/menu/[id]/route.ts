4de9405d3aef93c962141c092bd7f388c60d715f
Rizky Tri Mardiansyah <rizkytrimardiansyah08@gmail.com>
2025-06-09 15:59:01 +0700

    fix menu api

--- app/api/admin/menu/[id]/route.ts
+++ app/api/admin/menu/[id]/route.ts
@@ -1,18 +1,30 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
   const { name, description, price, categoryId } = await req.json();
+
   const menu = await prisma.menu.update({
-    where: { id: params.id },
+    where: { id },
     data: { name, description, price, categoryId },
   });
+
   return NextResponse.json(menu);
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
   await prisma.menu.delete({
-    where: { id: params.id },
+    where: { id },
   });
+
   return NextResponse.json({ success: true });
 }
