3395de42fb3ce1f9bee46b093ec27f6c076b02b3
Rizky Tri Mardiansyah <rizkytrimardiansyah08@gmail.com>
2025-06-09 16:49:57 +0700

    fix api transactions

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -2,22 +2,22 @@ import { NextResponse } from "next/server"
 import midtransClient from 'midtrans-client'
 
 const snap = new midtransClient.Snap({
-    isProduction:false,
-    serverKey:process.env.SECRET_MIDTRANS,
-    clientKey:process.env.NEXT_PUBLIC_MIDTRANS_CLIENT
-})
+  isProduction: false,
+  serverKey: process.env.SECRET_MIDTRANS || "",
+  clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT || ""
+});
 
 export async function POST(req: Request) {
-    const { id, totalAmount } = await req.json()
+  const { id, totalAmount } = await req.json();
 
-    const parameter = {
-        transaction_details: {
-            order_id: id,
-            gross_amount: totalAmount,
-        },
-    }
+  const parameter = {
+    transaction_details: {
+      order_id: id,
+      gross_amount: totalAmount,
+    },
+  };
 
-    const token = await snap.createTransactionToken(parameter)
-    console.log(token)
-    return NextResponse.json({ token })
+  const token = await snap.createTransactionToken(parameter);
+  console.log(token);
+  return NextResponse.json({ token });
 }
